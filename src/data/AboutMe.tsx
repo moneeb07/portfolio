7532223a1204b739512e4eee565c2c7b7fcc63c7
moneeb07 <moneeburrahman07@gmail.com>
2026-02-27 19:23:13 +0500

    about added2

--- src/data/AboutMe.tsx
+++ src/data/AboutMe.tsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
       <p>
         I’m a fast learner who adapts quickly to new technologies and applies them in real-world projects.
          I focus on strong fundamentals, clean architecture, and continuous improvement while building practical 
-         solutions. I don’t claim expertise without proper understanding — when I say I know a technology, it means I’ve
+         solutions. I don’t claim expertise without proper understanding, when I say I know a technology, it means I’ve
           worked with it deeply and can apply it confidently. If you want to build a website for your business or automate
            your daily tasks using AI, feel free to reach out.
       </p>
