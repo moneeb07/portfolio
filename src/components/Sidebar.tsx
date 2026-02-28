2b5f20116f893776b9564604ef2df97f155ca87a
moneeb07 <moneeburrahman07@gmail.com>
2026-02-28 11:41:09 +0500

    sidebar height issue fixed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -101,7 +101,7 @@ export default function Sidebar() {
             </div>
           </li>
 
-
+ 
           <li className="contact-item">
             <div className="icon-box">
               <IoLocationOutline />
